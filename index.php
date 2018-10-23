807052e97efea8b590633fef49f92423fabf0ef4
phamtunganh <anh.phamtung@vietis.com.vn>
2018-10-23 15:48:43 +0700

    Push

---
+++ index.php
@@ -0,0 +1,11 @@
+	<?php
+	$name = [
+		'firstname'  => 'John',
+		'lastname'   => 'Doe',
+		'middlename' => 'Bray'
+	];
+	
+	foreach ($name as $key => $value) {
+		echo $key . ':' . $value . '<br>';
+	}
+	?>
